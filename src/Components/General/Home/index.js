852aea5e3807d7a0c83ea420a1d0cec52da3d078
Shah Masood <58187558+ShahMasud@users.noreply.github.com>
2022-02-26 18:10:36 +0500

    sorting functionality

--- src/Components/General/Home/index.js
+++ src/Components/General/Home/index.js
@@ -1,47 +1,38 @@
 import React, { useState } from 'react'
-// import './style.css'
-import { Button, Container, Form, Table, Modal,Row, Col, Toast, FormControl } from 'react-bootstrap'
-// import { useNavigate } from 'react-router-dom';
+import './style.scss'
+import { Button, Container, Form, Table, Modal,Row, Col, Toast, FormControl, OverlayTrigger, Tooltip } from 'react-bootstrap'
 import { TiDelete} from "react-icons/ti";
 import { MdModeEditOutline} from "react-icons/md";
-import { useRef } from 'react';
 
 const Home = () => {
-    // states //
 
-    const [items, setItems]=useState([]);
+    // below are allt the states 👎
+
     const [data, setData]=useState({
         fname:"",
         lname:"",
         uname:""
     });
+    const [items, setItems]=useState([]);
 
     const [isEditItem, setIsEditItem]= useState("");
-//============================//
-// toast
-
     const [showw, setShoww] = useState(false);
-
-    //======================================//
-    // states and functions for modal
 
     const [show, setShow] = useState(false);
     const [aBtn, setABtn]= useState("");
     const [uBtn, setUBtn]= useState("");
 
+    // Now you are to functions world 👎
+
+    // Modal ftns 🥰
     const handleClose = () => setShow(false);
-    
     const handleShow = () =>{
          setShow(true);
          setABtn("add user");
          setUBtn("");
     }
 
-   
-    //============================================//
-
-
-    // code for input fields //
+    // you are here to input function 😄 
 
     const inputEvent = (event) =>{
         const {value, name}= event.target;
@@ -71,41 +62,35 @@ const Home = () => {
         } )
         }
 
-    // code for adding row dynamically in a table   //  
-
-    const addEmployee = (uname) =>{
+     
+    // function on "add user" button 👎
+    
+    const addUser = (uname) =>{
         setItems((olditems)=>{
             return [...olditems, data];
         });
-        
         setData({
             fname:"",
             lname:'',
             uname:""
         });
         setShow(false);
-
-        setShoww(true)
-        
-        
+        setShoww(true);   
     };
     
 
-    // code for delete row in a table //
+    // function on delete user 👎
 
     const deleteItem= (uname) =>{
-
        alert("are u sure to delete the user")
        console.log("deleted", uname);
        const arr=items.filter((arrElem, index)=>{
         return arrElem.uname !== uname;
-
     });
        setItems(arr);
-       
     }
 
-    // code for edit row in a table //
+    // function on edit user 👎
     
     const editRow =(uname)=>{
         let newEditItem=items.find((arrElem)=>{
@@ -119,51 +104,62 @@ const Home = () => {
             lname: newEditItem.lname,
             uname: newEditItem.uname
         });  
-
         setIsEditItem(INDEX);
         handleShow();
         setABtn("");
-        setUBtn("update");
-        
-        
+        setUBtn("update");  
     }
 
-    // code to update row in a table //
+    // function on update user 👎
 
     const updatItem =()=>{
         let a = items[isEditItem]=data;
-        // console.log(isEditItem);
         console.log(a);
         setData({
             fname:a.fname,
             lname:a.lname,
             uname: a.uname
         });
-
         setData({
             fname:"",
             lname:'',
             uname:""
         });
         setShow(false);
-       
     }
-   
+
+    // function to sort data on "first name" click 👎
+
+     const SortData= ()=>{
+       const a = items.sort(function(a, b) {
+            var nameA = a.fname.toUpperCase(); // ignore upper and lowercase
+            var nameB = b.fname.toUpperCase(); // ignore upper and lowercase
+            if (nameA < nameB) {
+              return -1;
+            }
+            if (nameA > nameB) {
+              return 1;
+            }
+            // names must be equal
+            return 0;
+            
+          });
+          setItems([a])
+          setItems(a);
+          console.log(items);
+       }
 
   return (
-    <Container className='p-0 shadow-sm main_Container mt-4'>
-             
-             <div className='Toast'>
+      <>
+      {/* Let's create a successfull toast after adding user 👎 */}
+
+          <div className='Toast'>
               <Row className='row'>
                 <Col xs={6}>
                     <Toast onClose={() => setShoww(false)} show={showw} delay={3000} autohide>
                     <Toast.Header>
-                        <img
-                        src="holder.js/20x20?text=%20"
-                        className="rounded me-2"
-                        alt=""
-                        />
-                        <strong className="me-auto">Bootstrap</strong>
+                       
+                        <strong className="me-auto">Hello!</strong>
                         <small>recently</small>
                     </Toast.Header>
                     <Toast.Body>user added successfully</Toast.Body>
@@ -171,23 +167,30 @@ const Home = () => {
                 </Col>
                 </Row>
              </div>
-             
 
-            {/* //============================= */}
-            <div className="d-flex align-items-center mb-2" style={{ margin:"0 auto"}}>
-            <Button className='bg-transparent text-black' onClick={handleShow}>add user</Button>
-            <Form className=''>
-                <FormControl
+
+        
+    {/* Main container 👎 */}
+
+    <Container className='p-3 shadow-sm main_Container mt-4'>
+             
+           <div className='p-2'>
+           <div className="headingDiv">
+            <div><Button className='addUBtn' onClick={handleShow}>add user</Button></div>
+            <div>
+            <Form >
+                <FormControl className="a"
                 type="search"
                 placeholder="Search"
                 className="me-2"
                 aria-label="Search"
                 />
-                {/* <Button variant="outline-success">Search</Button> */}
             </Form>
+            </div>
             </div>
            
            
+            {/* Let's create a modal when we click on the above "add user" button 👎 */}
 
             <Modal show={show} onHide={handleClose}>
                 <Modal.Header closeButton>
@@ -212,47 +215,92 @@ const Home = () => {
                 <Modal.Footer>
                 <Button variant="secondary" onClick={handleClose}>Close</Button>
                 {(uBtn)?<Button onClick={updatItem}>{uBtn}</Button>:null}
-                {(aBtn)?<Button variant="primary" onClick={addEmployee}>{aBtn}</Button>:null}
+                {(aBtn)?<Button variant="primary" onClick={addUser}>{aBtn}</Button>:null}
     
                 </Modal.Footer>
             </Modal>
-            {/* ========================================== */}
 
 
+            {/* Let's create a Table 👎 */}
 
-            <Table striped bordered hover size="sm">
+            <Table className='theTable' striped bordered hover size="sm">
                 <thead>
                     <tr>
                     <th>#</th>
-                    <th>First Name</th>
+                    <th>
+                    <OverlayTrigger overlay={<Tooltip id="tooltip-disabled">Sort</Tooltip>}>
+                        <span className="">
+                            <th onClick={ ()=>SortData()} className='Name'>First Name</th>
+                        </span>
+                    </OverlayTrigger> 
+                    </th>
+                    
                     <th>Last Name</th>
                     <th>Username</th>
-                    <th>Actions</th>
+                    <th className='text-center'>Actions</th>
                     </tr>
                 </thead>
-
+                <tbody>
                 {items.map((itemval, index)=>{
                     return(
-                            <tbody>
+                            
                             <tr key={itemval?.uname}>
-                            <td>{index}</td>
-                            <td>{itemval?.fname}</td>
+                            <td>{index+1}</td>
+                            <td >{itemval?.fname}</td>
                             <td>{itemval?.lname}</td>
                             <td>{itemval?.uname}</td>
-                            <td> 
-                            <div className='d-flex  justify-content-end gap-4' style={{color:"red", width:"100%"}}>
-                            <div className='d-flex rounded-circle bg-white' style={{width:"35px", height:"35px", fontSize:"35px"}}>
-                            <TiDelete className='delete' onClick={()=> {deleteItem(itemval.uname)}}/>
+                            <td>
+
+                            <div className='d-flex justify-content-center gap-4' style={{color:"red", width:"100%"}}>
+                                <div className='d-flex align-items-center justify-content-center' style={{width:"30px", height:"30px", fontSize:"25px"}}>
+                                    <OverlayTrigger overlay={<Tooltip id="tooltip-disabled">Delete</Tooltip>}>
+                                        <span className="">
+                                            <TiDelete className='d-flex delete' onClick={()=> {deleteItem(itemval.uname)}}/>
+                                        </span>
+                                    </OverlayTrigger>
+                                </div>
+                                <div>
+                                    <OverlayTrigger overlay={<Tooltip id="tooltip-disabled">Edit</Tooltip>}>
+                                        <span className="">
+                                        <MdModeEditOutline className='text-warning edit' onClick={()=>editRow(itemval.uname, index)}/>
+                                        </span>
+                                    </OverlayTrigger> 
+                                </div>
                             </div>
-                            <MdModeEditOutline className='text-warning edit' onClick={()=>editRow(itemval.uname, index)}/>
-                            </div>
+
                             </td>
-                            </tr>
-                            </tbody>
+                            </tr>  
                         )
                     })}
+                    </tbody>
                 </Table>
+
+
+                {/* Let's start coding for Pagination 👎 */}
+
+                   <div className='footer'>
+                   <p>Showing 5 out of 25 entries</p>
+                    <nav aria-label="Page navigation example">
+                        <ul className="pagination">
+                            <li className="page-item">
+                            <a className="page-link" href="#" aria-label="Previous">
+                                <span aria-hidden="true">&laquo;</span>
+                            </a>
+                            </li>
+                            <li className="page-item"><a className="page-link" href="#">1</a></li>
+                            <li className="page-item"><a className="page-link" href="#">2</a></li>
+                            <li className="page-item"><a className="page-link" href="#">3</a></li>
+                            <li className="page-item">
+                            <a className="page-link" href="#" aria-label="Next">
+                                <span aria-hidden="true">&raquo;</span>
+                            </a>
+                            </li>
+                        </ul>
+                    </nav>
+                   </div>
+           </div>
     </Container>
+    </>
   )
 }
 
